abd4af0dcec6b7648b6bc6f7c19db101c6e27592
Mickael MAS <mickael.mas@appiwedia.com>
2020-02-19 12:30:31 +0100

    Initial Commit

---
+++ TODOList/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TODOList
+//
+//  Created by Mickael Mas on 19/02/2020.
+//  Copyright © 2020 APPIWEDIA. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
